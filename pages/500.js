441756a79ef1b1eccc1aaea51419effde2824050
Mukesh Dhungana <Mukesh0702>
2021-04-28 23:01:37 +0545

    add 505 page

---
+++ pages/500.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function InternalServerError(props) {
+    return (
+        <div>
+            This is 500 page
+        </div>
+    );
+}
+
+export default InternalServerError;This is 505 page
